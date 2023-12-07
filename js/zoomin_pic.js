0f6771fd3890a82eecb6971ea7f316d32db61c35
Jonas Syed <j.syed@gmx.de>
2023-12-07 18:31:33 +0100

    Bugfix zoomin images

--- js/zoomin_pic.js
+++ js/zoomin_pic.js
@@ -1,13 +1,13 @@
 function zoomin_pic(unique_id) {
 	// Get the modal
-    id_modal = "myModal" + unique_id;
-    id_img = "img" + unique_id;
+    id_modal = "myModal" + unique_id.toString();
+    id_img = "img" + unique_id.toString();
 	var modal = document.getElementById(id_modal);
 
 	// Get the image and insert it inside the modal - use its "alt" text as a caption
-	var img = document.getElementsByClassName("myImg")[0];
+	var img = document.getElementsByClassName("myImg")[unique_id-1];
 	var modalImg = document.getElementById(id_img);
-	var captionText = document.getElementsByClassName("caption")[0];
+	var captionText = document.getElementsByClassName("caption")[unique_id-1];
 	img.onclick = function(){
 		modal.style.display = "block";
 		modalImg.src = this.src;
@@ -15,7 +15,7 @@ function zoomin_pic(unique_id) {
 	};
 
 	// Get the <span> element that closes the modal
-	var span = document.getElementsByClassName("close")[0];
+	var span = document.getElementsByClassName("close")[unique_id-1];
 
 	// When the user clicks on <span> (x), close the modal
 	span.onclick = function() {
