ac0e8d324ba9f9fd0381dbe12998d9bf7bbbc9b7
Jonas Syed <j.syed@gmx.de>
2024-01-30 18:35:15 +0100

    Code cleanup

--- js/setsize.js
+++
@@ -1,9 +0,0 @@
-$(window).load(function slider() {
-  var elems = $(".slideIn");
-  var h = 0;
-  $(elems).each(function () {
-    h += $(this).height();
-  });
-  //console.log(h);
-  $("#itemSlider").height(h);
-});
--- js/slidein_mobile.js
+++
@@ -1,6 +0,0 @@
-$(window).load(function slider_mobile() {
-    //console.log(_5grid.isMobile);
-    if (_5grid.isMobile) {
-        $(".slideIn").show();
-    }
-});
