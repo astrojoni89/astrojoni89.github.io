434d0b6b43baf52e70d9279cd879831c99c998ee
Jonas Syed <j.syed@gmx.de>
2024-02-12 18:21:50 +0100

    Update mobile version

--- js/magneticbutton.js
+++ js/magneticbutton.js
@@ -1,30 +1,34 @@
 // get the button and animate its position based on the mouse position
-const button = document.querySelector('.buttonADS2');
-let boundingRect = button.getBoundingClientRect();
+$(window).load(function () {
+  if (_5grid.isDesktop) {
+    const button = document.querySelector(".buttonADS2");
+    let boundingRect = button.getBoundingClientRect();
 
-window.addEventListener('resize', () => {
-    boundingRect = button.getBoundingClientRect();
-});
+    window.addEventListener("resize", () => {
+      boundingRect = button.getBoundingClientRect();
+    });
 
-button.addEventListener('mousemove', (e) => {
-    const mousePosX = e.pageX - boundingRect.left;
-    const mousePosY = e.pageY - boundingRect.top;
+    button.addEventListener("mousemove", (e) => {
+      const mousePosX = e.pageX - boundingRect.left;
+      const mousePosY = e.pageY - boundingRect.top;
 
-    // define destination values of button
-    gsap.to(button, {
+      // define destination values of button
+      gsap.to(button, {
         x: (mousePosX - boundingRect.width / 2) * 0.1,
         y: (mousePosY - boundingRect.height / 2) * 0.05,
         duration: 0.8,
-        ease: 'power3.out',
+        ease: "power3.out",
+      });
     });
-});
 
-button.addEventListener('mouseleave', () => {
-    gsap.to(button, {
+    button.addEventListener("mouseleave", () => {
+      gsap.to(button, {
         x: 0,
         y: 0,
         duration: 0.8,
-        ease: 'elastic.out(1,0.3)',
+        ease: "elastic.out(1,0.3)",
+      });
     });
+  }
 });
 
